22e7ec158e2f750b4c55329dca96a3752e21c679
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2023-07-07 22:45:41 +0530

    header-partial-animation

---
+++ index.js
@@ -0,0 +1,15 @@
+const links = document.getElementsByClassName('nav-link');
+
+
+[...links].forEach((ele)=>{
+    ele.addEventListener('mouseover', function(eve){
+        const tar = eve.target.parentNode.children[1].style; 
+        tar.animation = 'slide 0.3s ease-in';
+        tar.width = "100%";
+    });
+    ele.addEventListener('mouseout', function(eve){
+        const tar = eve.target.parentNode.children[1].style; 
+        tar.animation='';
+        tar.width = '0';
+    })
+})
